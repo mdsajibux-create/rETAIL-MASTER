03e410213f151b97e1cf6959da1900e50cfdb2e2
mdsajibux-create <mdsajibux@gmail.com>
2026-06-30 01:08:18 +0600

    fix: explicit relative path for next-intl plugin

--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -1,59 +1,10 @@
-// next.config.mjs
-import createNextIntlPlugin from "next-intl/plugin";
-import fs from "fs";
-import path from "path";
-import bundleAnalyzer from "@next/bundle-analyzer";
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin("./src/i18n.tsx");
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
-const imageHost = process.env.NEXT_PUBLIC_IMAGE_HOST ?? "192.168.88.225:8000";
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-export default withBundleAnalyzer(
-  withNextIntl({
-    reactStrictMode: false,
-    trailingSlash: process.env.TRAILING_SLASH === "true",
-    poweredByHeader: false,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your existing nextConfig options (if you have any) go here
+};
 
-    output: "standalone",
-
-    distDir: "build",
-    experimental: {
-      staleTimes: {
-        dynamic: 30,
-      },
-    },
-    images: {
-      unoptimized: true,
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "res.cloudinary.com",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: imageHost,
-          pathname: "/storage/**",
-        },
-        {
-          protocol: "http",
-          hostname: "localhost",
-          pathname: "/storage/**",
-        },
-        {
-          protocol: "http",
-          hostname: "192.168.88.225",
-          port: "8000",
-          pathname: "/storage/**",
-        },
-      ],
-    },
-
-    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-    compiler: {
-      removeConsole: process.env.NODE_ENV === "production",
-    },
-  }),
-);
+export default withNextIntl(nextConfig);
